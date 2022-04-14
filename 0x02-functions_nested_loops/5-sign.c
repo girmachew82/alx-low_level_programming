c0da87a68bb48a19a9d19f73709030ca930dbd91
girmachew82 <girmachewabebe>
2022-04-14 07:09:32 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign -return sign 
+ *
+ * @n: get input
+ * Return : Return sign +, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
