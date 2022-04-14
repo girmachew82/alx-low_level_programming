612103c39d4bd97e905677dd0481b33aa1434c1e
girmachew82 <girmachewabebe>
2022-04-13 20:05:41 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
@@ -15,21 +14,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 #endif
-=======
-#ifndef _MAIN_H_                                                               #define _MAIN_H_                                                               
-                                                                               
-int _putchar(char c);                                                          
-void print_alphabet(void);                                                     
-void print_alphabet_x10(void);                                                 
-int isLower(int n);                                                            
-int isalpha(int n);                                                            
-int print_sign(int n);                                                         
-int _abs(int);                                                                 
-int print_last_digit(int);                                                     
-void jack_bauer(void);                                                         
-void times_table(void);                                                        
-int add(int, int);                                                             
-void print_to_98(int n);                                                       
-void print_times_table(int n);                                                 
-#endif     
->>>>>>> 0df9a88b00cb8e1451a715cf465a0d92785e4aac
