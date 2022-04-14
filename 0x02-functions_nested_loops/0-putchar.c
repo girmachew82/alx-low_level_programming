1dd0b75c79bdf13dfd01e541e3fb65dd22294a57
girmachew82 <girmachewabebe>
2022-04-13 19:58:53 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <unistd.h>
+#include "min.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * main -entry point
+ * 
+ * Return: o
+ */ 
+ int main(void)
 {
-	return (write(1, &c, 1));
+	char pcr[] = '_putchar';
+	
+	int c;
+	
+	for(c = 0;  c < 8; c++)
+	{
+		_putchar(pcr[c]);
+	}
+
+	_putchar('\n');
+	return(0);
 }
