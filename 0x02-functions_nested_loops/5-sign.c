27a413f3b40c0693423053094bbd2d4eff813634
girmachew82 <girmachewabebe>
2022-04-14 11:21:40 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,25 @@
  * Return : Return sign +, 0 or -1
  */
 
+
 int print_sign(int n)
 {
+	int number;
+
 	if (n > 0)
-		return (1);
+	{
+		_putchar('+');
+		number = 1;
+	}
 	else if (n == 0)
-		return (0);
+	{
+		_putchar('0');
+		number = 0;
+	}
 	else
-		return (-1);
+	{
+		_putchar('-');
+		number = -1;
+	}
+	return (number);
 }
