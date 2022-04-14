1f3dbcd71e3a60aaaede08ba48d76932a03764ad
girmachew82 <girmachewabebe>
2022-04-14 02:36:21 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-/**
- * main -entry point
- * 
- * Return: o
- */ 
- int main(void)
-{
-	char pcr[] = "_putchar";
-	
-	int c;
-	
-	for(c = 0;  c < 8; c++)
-	{
-		_putchar(pcr[c]);
-	}
+/** 
+ * main - entry point 
+ * @void no argument
+ * Retuen 0
+ * */
 
+int main(void)
+{
+	char crs[] = "_putchar";
+
+	int c;
+
+	for(c = 0; c < 8; c++)
+	{
+		_putchar(crs[c]);
+	}
 	_putchar('\n');
-	return(0);
+	return (0);
 }
