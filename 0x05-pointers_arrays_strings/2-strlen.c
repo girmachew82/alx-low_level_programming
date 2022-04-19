192b0bb381cae89237b6355ba61eeeb356164a0a
girmachew82 <girmachewabebe>
2022-04-19 00:49:45 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strlen -  a function that returns the length of a string.
+ * _strlen -  a function that returns the length of a string.
  * @s: An input string
  * Return: Nothing
  */
