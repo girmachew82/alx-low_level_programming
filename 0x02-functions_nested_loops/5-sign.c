b9aa34385fe0377f0a642b5b133a9775357d85d8
girmachew82 <girmachewabebe>
2022-04-19 05:39:51 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
-/**
- * print_sign - return sign 
- * @n: integer
- * Return: Return sign +, 0 or -1
- */
-int print_sign(int n)
-{
-	int number;
-
-	if (n > 0)
-	{
-		_putchar('+');
-		number = 1;
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		number = 0;
-	}
-	else
-	{
-		_putchar('-');
-		number = -1;
-	}
-	return (number);
-}
+/**                                                                     
+ * print_sign - return sign                                             
+ * @n: integer                                                          
+ * Return: Return sign +, 0 or -1                                       
+ */                                                                     
+int print_sign(int n)                                                   
+{                                                                       
+	int number;                                                     
+                                                                        
+        if (n > 0)                                                      
+        {                                                               
+        	_putchar('+');                                          
+		number = 1;                                             
+        }                                                               
+        else if (n == 0)                                                
+        {                                                               
+               _putchar('0');                                          
+               number = 0;                                             
+        }                                                               
+        else                                                            
+        {                                                               
+               _putchar('-');                                          
+               number = -1;                                            
+        }                                                               
+        return (number);                                                
+} 
