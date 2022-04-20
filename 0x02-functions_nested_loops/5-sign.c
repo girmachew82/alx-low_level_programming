da5a778337115f065b21beeaf2fb2ce2f4b05c58
girmachew82 <girmachewabebe>
2022-04-20 02:23:08 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_sign - return sign 
- * @n: get input
- * Return: -1, 1, 0
+ *print_sign - return sign 
+ *@n: get input
+ *Return: -1, 1, 0
  */
 int print_sign(int n)
 {
 	int number;
 
 	if (n > 0)
-	{	
+	{
 		number = 1;
 		_putchar('+');
 	}
 	else if (n == 0)
-	{	
+	{
 		number = 0;
 		_putchar('0');
 	}
 	else
-	{	
+	{
 		number = -1;
 		_putchar('-');
 	}
