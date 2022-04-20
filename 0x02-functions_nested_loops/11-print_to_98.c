743ea608bf87efc71fd71d1f4a67bcbd1a5b24b7
girmachew82 <girmachewabebe>
2022-04-20 04:40:19 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,24 +3,25 @@
 
 /**
  * print_to_98 - print numbers to 98
+ * @n: an integer
  * Return: nothing
  */
 void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n <= 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
-	else if ( n == 98)
+	else if (n == 98)
 		printf("%d", 98);
 	else
 	{
-		for (; n >= 98; n++)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
 			_putchar(',');
