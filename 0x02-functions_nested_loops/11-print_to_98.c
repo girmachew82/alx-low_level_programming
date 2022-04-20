936b2ca1e2d245a32f04021e02aa3099f69c331f
girmachew82 <girmachewabebe>
2022-04-20 05:58:07 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,20 +12,20 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d ", n);
+			printf("%d", n);
 			if (n == 98)
 				break;
-			printf(",");
+			printf(", ");
 		}
 	}
 	else
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d ", n);
+			printf("%d", n);
 			if (n == 98)
 				break;
-			printf(",");
+			printf(", ");
 		}	
 	}
 }
