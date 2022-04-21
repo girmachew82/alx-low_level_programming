ad4b6bc04d114e3545a4d3ca77aff9abcfc7e03b
girmachew82 <girmachewabebe>
2022-04-20 23:21:16 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,8 +15,10 @@ void print_to_98(int n)
 			printf("%d", n);
 			if (n == 98)
 				continue;
-			printf(", ");
+			printf(",");
+			printf(" ");
 		}
+		printf("\n");
 	}
 	else
 	{
@@ -25,7 +27,9 @@ void print_to_98(int n)
 			printf("%d", n);
 			if (n == 98)
 				continue;
-			printf(", ");
+			printf(",");
+			printf(" ");
 		}	
+		printf("\n");	
 	}
 }
