f963cf6902eb9fdd759306cf3b83c639a7a44bec
girmachew82 <girmachewabebe>
2022-04-23 10:25:43 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: parameter1
+ *
+ * Return: The number
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar((n * -1) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+}
