4fb76ca06d740e1f10d1e3efc29adb7c5395a197
girmachew82 <girmachewabebe>
2022-04-25 15:03:40 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	int t1 = 1, t2 =2, i;
-	long next  = t1 + t2;
-	
+	long int  next  = t1 + t2;
+		
 	printf("%d, %d, ", t1, t2);
 
 	for (i = 3; i <= 50; i++)
 	{
-		printf("%ld, ", next);
+		printf("%lu, ", next);
 		t1 = t2;
 		t2 = next;
 		next = t1 + t2;
